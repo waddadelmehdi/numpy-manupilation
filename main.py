810b3e1f8cf15a536f6fc6f2c65e1c084d51c61a
WADDAD EL MEHDI <waddadelmehdi@gmail.com>
2025-03-24 14:55:53 +0000

    Continue at home

--- main.py
+++ main.py
@@ -41,6 +41,54 @@ print(x.shape)
 #print(x)
 #iteration sur les elemets de x afficher un par un
 y=np.empty(x.shape,x.dtype)
-#for item in x:
-#   print(item)
+# i=0
+# for item in x:
+#     y[i]=item**2
+#     i+=1
+#y=x**-2*np.sin(x)
+y=np.exp(-x**2/10)*np.sin(15*x)
+print(y[:10]) #Extration d'un tranch d'un vector
+plt.plot(x,y)
+plt.show()
+# %%
+x=np.arange(0,100)
+x[0]
+x[10:20]
+x[:20]
+x[90:]
+x[1:100:2]
+x[100:1:-2]
+# %%
+x[0]=10010
+x[0:5]=np.array([-3,6,-7,9,5])
+print(x)
+# %%
+print(x[-1])
+# %%
+print(x[-4:-1:1])
+# %%
+print(x[-1:-4:-1])
+# %%
+x=np.random.randint(100,size=10)
+np.random.shuffle(x) 
+print(x)
+# %%
+#m1=np.zeros((4,4)) #matrice tous elems 0
+#m1=np.ones((4,4)) #matrice tous elems 1
+m1=np.identity(10)
+m1
+# %%
+a=np.random.randint(20,50)*np.random.rand(10,10)
+y=np.random.randint(20,50)*np.random.rand(10)
+print(y)
+inv_a=np.linalg.inv(a)
+m1
+print(np.dot(m1,inv_a))
+sol=np.dot(m1,y)
+print(sol)
+#verification
+err=np.sum((np.dot(m1,sol)-y)**2)
+sol2=np.linalg.solve(m1,y)
+err2=np.sum((np.dot(m1,sol2)-y)**2)
+print('err=',err,'err2=',err2)
 # %%
